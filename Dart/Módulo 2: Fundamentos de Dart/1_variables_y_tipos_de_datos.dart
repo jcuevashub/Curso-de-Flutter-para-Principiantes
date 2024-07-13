3a1b3cc025630f1150989cf28417139fa7a19776
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-07-13 09:16:36 -0400

    Update 1_variables_y_tipos_de_datos.dart

--- Dart/Módulo 2: Fundamentos de Dart/1_variables_y_tipos_de_datos.dart
+++ Dart/Módulo 2: Fundamentos de Dart/1_variables_y_tipos_de_datos.dart
@@ -1,9 +1,29 @@
-void main() {
-  int edad = 25;
-  double altura = 1.75;
-  String nombre = 'Juan';
-  bool esEstudiante = true;
+/*
+Una variable en Dart es un contenedor que se utiliza para almacenar datos.
+Puedes declarar variables usando las palabras clave var, final, o const, dependiendo de cómo quieras que se comporte la variable.
+*/
 
-  print(
-      'Nombre: $nombre, Edad: $edad, Altura: $altura, Es estudiante: $esEstudiante');
+//var: Se utiliza para declarar una variable cuyo valor puede cambiar.
+
+//final: Se utiliza para declarar una variable cuyo valor no puede cambiar una vez asignado.
+
+//const: Se utiliza para declarar una variable constante que debe conocerse en tiempo de compilación.
+
+void main() {
+  var nombre = 'Juan';
+  nombre = 'Carlos'; // Correcto
+
+  final apellido = 'Perez';
+  //apellido = 'Cuevas'; // Error: no se puede reasignar una variable final
+
+  const pi = 3.14159;
+  // pi = 3.14; // Error: no se puede reasignar una variable const
+
+  // int edad = 25;
+  // double altura = 1.75;
+  // String nombre2 = 'Juan';
+  // bool esEstudiante = true;
+
+  // print(
+  //     'Nombre: $nombre, Edad: $edad, Altura: $altura, Es estudiante: $esEstudiante');
 }
